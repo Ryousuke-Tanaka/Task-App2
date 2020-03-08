5ee7dd9945d6858adf5b5f0fb022b5e97846d6f3
Ryousuke-Tanaka <sample@email.com>
2020-03-08 02:42:35 +0000

    B8トップページ完了

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,16 @@
 # coding: utf-8
 
-User.create!( name: "Sample User",
+User.create!( name: "管理者",
               email: "sample@email.com",
+              password: "password",
+              password_confirmation: "password")
+
+User.create!( name: "ゲスト管理者",
+              email: "guest-admin@email.com",
+              password: "password",
+              password_confirmation: "password")
+
+User.create!( name: "ゲストユーザー",
+              email: "guest@email.com",
               password: "password",
               password_confirmation: "password")
